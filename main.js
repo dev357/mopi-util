dbb3b5ce9cf96052e1a0703aa30b952894f0e8c9
Risto Laidla <risto@lakrito.local>
2016-02-04 22:19:28 +0200

    moved config options into config object

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-'use strict'
+'use strict';
+
 
 // CONFIG - edit these ########################################################################
-const battery = 1; // which port on the MoPi is the backup battery connected to
-const mainPower = 2; // which port on the MoPi is the main power connected to
-const timer = 11; // how long should we wait before deciding the main power is gone for good
-const mainLowVoltage = 10; // when the main battery drops below this, start timer
-const pollingInterval = 2; // how often should we check if voltage has dropped, in s
-const shutdownDelay = 35; // how long to wait until power is cut, in seconds(min 30s)
+const config = {
+  battery: 1, // which port on the MoPi is the backup battery connected to
+  mainPower: 2, // which port on the MoPi is the main power connected to
+  timer: 11,  // how long should we wait before deciding the main power is gone for good
+  mainLowVoltage: 10,  // when the main battery drops below this, start timer
+  pollingInterval: 2,  // how often should we check if voltage has dropped, in s
+  shutdownDelay: 35  // how long to wait until power is cut, in seconds(min 30s)
+}
 // ############################################################################################
 
 // DO NOT EDIT BEYOND THIS POINT
@@ -19,20 +22,20 @@ mopi.connect(function(err) {
   if (err) return console.log(err);
 
   function isMainVoltageGood() {
-    return new Promise((resolve, reject) => {
-      mopi.getVoltage(mainPower, (err, voltage) => {
+    return new Promise((resolve) => {
+      mopi.getVoltage(config.mainPower, (err, voltage) => {
         if (err) {
           console.log('checkVoltage error', err);
           resolve(false);
         }
         console.log('voltage: ' + voltage);
-        if (voltage < mainLowVoltage * 1000) resolve(false);
+        if (voltage < config.mainLowVoltage * 1000) resolve(false);
         resolve(true);
       });
     });
   }
 
-  const timerCounter = Math.ceil(timer / pollingInterval);
+  const timerCounter = Math.ceil(config.timer / config.pollingInterval);
   let shutdownCounter = 0;
 
   function shouldShutdown() {
@@ -63,7 +66,7 @@ mopi.connect(function(err) {
   function doShutdown() {
     clearInterval(shutdownInterval);
     console.log('SHUTTING DOWN!');
-    let delay = shutdownDelay - 30 < 0 ? 0 : shutdownDelay - 30;
+    let delay = config.shutdownDelay - 30 < 0 ? 0 : config.shutdownDelay - 30;
     mopi.setShutdownDelay(delay, (err) => {
       if (err) return console.log("Error", err);
       mopi.getShutdownDelay((err, value) => {
@@ -75,5 +78,5 @@ mopi.connect(function(err) {
     exec("shutdown -h now");
   }
 
-  let shutdownInterval = setInterval(shouldShutdown, pollingInterval * 1000);
+  let shutdownInterval = setInterval(shouldShutdown, config.pollingInterval * 1000);
 });
